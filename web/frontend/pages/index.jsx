05d3fda5ec8cac965c77fb9e525cc043c5e1f3fe
yusuf saif <yusuf.saif@cloudanalogy.com>
2022-07-15 16:38:07 +0530

    msg send

---
+++ web/frontend/pages/index.jsx
@@ -0,0 +1,21 @@
+import {
+  Card,
+  Page,
+  Layout,
+  TextContainer,
+  Image,
+  Stack,
+  Link,
+  Heading,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+
+import { trophyImage } from "../assets";
+
+import { ProductsCard } from "../components";
+
+export default function HomePage() {
+  return (
+  <h1>React view</h1>
+  );
+}
